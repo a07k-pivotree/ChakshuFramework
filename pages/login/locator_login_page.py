f580a99d27beaa9f2f14daffd98066a6acbc34b6
harshjain-QA <harsh.jain@pivotree.com>
2026-03-27 15:21:41 +0530

    username code add

--- pages/login/locator_login_page.py
+++ pages/login/locator_login_page.py
@@ -1,1 +1,30 @@
 # This file contain path of elements
+
+class LoginLocators:
+    @staticmethod
+    def sign_in_link():
+        return "//nav//span[text()='Sign In']"
+
+    @staticmethod
+    def username_dropdown():
+        return "div#username"
+
+    @staticmethod
+    def username_option(username_text):
+        return f"div:has-text('{username_text}')"
+
+    @staticmethod
+    def password_dropdown():
+        return "div#password"
+
+    @staticmethod
+    def password_option(password_text):
+        return f"div:has-text('{password_text}')"
+
+    @staticmethod
+    def login_button():
+        return "#login-btn"
+
+    @staticmethod
+    def logout_link():
+        return "//nav//span[text()='Logout']"
