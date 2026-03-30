2c9cbde1ed7d033bb8e8a6a59a5bf829a243d0f5
harshjain-QA <harsh.jain@pivotree.com>
2026-03-30 17:36:08 +0530

    login test complete

--- pages/login/login_page.py
+++ pages/login/login_page.py
@@ -1,14 +1,23 @@
 # Include function for the
 from pages.login.locator_login_page import LoginLocators
-
 class LoginPage:
-    def __int__(self,page):
+    def __init__(self,page):
         self.page = page
 
-    def click_sign_in_menue(self):
-        self.page.locator(LoginLocators.sign_in_link()).click()
+    def click_sign_in_menu(self):
+        self.page.locator(LoginLocators.sign_in_link).click()
 
     def select_username(self,username):
-        self.page.locator(LoginLocators.username_dropdown()).click()
+        self.page.locator(LoginLocators.username_dropdown).click()
         self.page.locator(LoginLocators.username_option(username)).click()
+
+    def select_password(self,password):
+        self.page.locator(LoginLocators.password_dropdown).click()
+        self.page.locator(LoginLocators.password_option(password)).click()
+
+    def click_login(self):
+        self.page.locator(LoginLocators.login_button).click()
+
+    def get_logout(self):
+        return self.page.locator(LoginLocators.logout_link)
 
